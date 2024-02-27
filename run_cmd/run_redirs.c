9f4b1766d86b9cccceca427c3eaaf6d2101e115c
Omar Tourabi <otourabi@c2r7p1.42lehavre.fr>
2024-02-27 15:53:25 +0100

    start of ctrl-d of heredoc

--- run_cmd/run_redirs.c
+++ run_cmd/run_redirs.c
@@ -1,22 +1,35 @@
 #include "../minishell.h"
 
-void save_heredoc(t_redirs *cmd, t_list **envp, char *tmp_file)
+void	handle_heredoc(int sig)
 {
-	int fd;
-	char *line;
+	if (sig == SIGINT)
+	{
+		write(1, "\n", 1);
+		exit(0);
+	}
+	if (sig == SIGQUIT)
+	{
+
+	}
+}
+
+void	save_heredoc(t_redirs *cmd, t_list **envp, char *tmp_file)
+{
+	int		fd;
+	char	*line;
 
 	(void)envp;
-	fd = open(tmp_file, O_WRONLY | O_APPEND | O_CREAT, 0777);
+	unlink(tmp_file);
+	fd = open(tmp_file, O_WRONLY | O_CREAT, 0777);
 	while (1)
 	{
-		// write(STDOUT_FILENO, "heredoc> ", 10);
-		line = readline("heredoc> "); // get_next_line(0);
-		/* if (ft_strlen(line) > 0)
-			line[ft_strlen(line) - 1] = 0; */
+		signal(SIGINT, handle_heredoc);
+		signal(SIGQUIT, handle_heredoc);
+		line = readline("heredoc> ");
 		if (line && ft_strcmp(line, cmd->filename) == 0)
 		{
 			free(line);
-			break;
+			break ;
 		}
 		write(fd, line, ft_strlen(line));
 		write(fd, "\n", 1);
@@ -27,9 +40,9 @@ void save_heredoc(t_redirs *cmd, t_list **envp, char *tmp_file)
 	close(fd);
 }
 
-void run_sub_redirs(t_cmd *c, t_list **envp, int prev_fd)
+void	run_sub_redirs(t_cmd *c, t_list **envp, int prev_fd)
 {
-	t_redirs *cmd;
+	t_redirs	*cmd;
 
 	cmd = (t_redirs *)c;
 	if (cmd->is_here_doc)
@@ -49,9 +62,9 @@ void run_sub_redirs(t_cmd *c, t_list **envp, int prev_fd)
 		exec_cmd_by_type(cmd->cmd, envp);
 }
 
-void run_multiple_heredoc(t_redirs *cmd, t_list **envp)
+void	run_multiple_heredoc(t_redirs *cmd, t_list **envp)
 {
-	t_redirs *next;
+	t_redirs	*next;
 
 	if (cmd->cmd->type == REDIR_CMD)
 	{
@@ -63,11 +76,11 @@ void run_multiple_heredoc(t_redirs *cmd, t_list **envp)
 	unlink("__fake__");
 }
 
-void update_cmd(t_cmd **cmd)
+void	update_cmd(t_cmd **cmd)
 {
-	t_redirs *init;
-	t_redirs *next_cmd;
-	t_redirs *to_remove;
+	t_redirs	*init;
+	t_redirs	*next_cmd;
+	t_redirs	*to_remove;
 
 	init = (t_redirs *)*cmd;
 	next_cmd = (t_redirs *)init->cmd;
@@ -81,7 +94,7 @@ void update_cmd(t_cmd **cmd)
 	init->cmd = (t_cmd *)next_cmd;
 }
 
-void run_redirs(t_cmd *c, t_list **envp, int run_next)
+void	run_redirs(t_cmd *c, t_list **envp, int run_next)
 {
 	t_redirs *cmd;
 	t_redirs *next;
