b267062071c7a860d0ef0c31941122e6d5f64c0e
Nicolas Courtois <nicolascourtois3@gmail.com>
2024-02-28 12:49:30 +0100

    norminette

--- env/build_env.c
+++ env/build_env.c
@@ -1,40 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   build_env.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncourtoi <ncourtoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 10:18:13 by ncourtoi          #+#    #+#             */
+/*   Updated: 2024/02/28 10:22:45 by ncourtoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-static char *set_env_str(t_var var)
+static char	*set_env_str(t_var var)
 {
-    char *env_str;
+	char	*env_str;
 
-    env_str = ft_calloc(ft_strlen(var.key) + ft_strlen(var.data) + 2, sizeof(char));
-    if (!env_str)
-        return (NULL);
-    ft_strcat(env_str, var.key);
-    ft_strcat(env_str, "=");
-    ft_strcat(env_str, var.data);
-    return (env_str);
+	env_str = ft_calloc(ft_strlen(var.key) + ft_strlen(var.data) + 2,
+			sizeof(char));
+	if (!env_str)
+		return (NULL);
+	ft_strcat(env_str, var.key);
+	ft_strcat(env_str, "=");
+	ft_strcat(env_str, var.data);
+	return (env_str);
 }
 
-char **build_env(t_list *env_var)
+char	**build_env(t_list *env_var)
 {
-    char **envp;
-    int i;
-    char *env_str;
-    t_var var;
+	char	**envp;
+	int		i;
+	char	*env_str;
+	t_var	var;
 
-    envp = ft_calloc(ft_listsize(env_var), sizeof(char *));
-    if (!envp)
-        return (NULL);
-    i = 0;
-    while (env_var)
-    {
-        var = *(t_var *)env_var->content;
-        if (!ft_strcmp(var.key, "?") || !ft_strcmp(var.key, "_LAST_EXIT_") || !var.data)
-        {
-            env_var = env_var->next;
-            continue;
-        }
-        env_str = set_env_str(var);
-        envp[i++] = env_str;
-        env_var = env_var->next;
-    }
-    return (envp);
+	envp = ft_calloc(ft_listsize(env_var), sizeof(char *));
+	if (!envp)
+		return (NULL);
+	i = 0;
+	while (env_var)
+	{
+		var = *(t_var *)env_var->content;
+		if (!ft_strcmp(var.key, "?") || !ft_strcmp(var.key, "_LAST_EXIT_")
+			|| !var.data)
+		{
+			env_var = env_var->next;
+			continue ;
+		}
+		env_str = set_env_str(var);
+		envp[i++] = env_str;
+		env_var = env_var->next;
+	}
+	return (envp);
 }
--- env/handle_env.c
+++ env/handle_env.c
@@ -1,124 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_env.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncourtoi <ncourtoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 10:18:16 by ncourtoi          #+#    #+#             */
+/*   Updated: 2024/02/28 10:18:17 by ncourtoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-int add_var(t_list **var_list, char *key, char *value)
+int	add_var(t_list **var_list, char *key, char *value)
 {
-    t_var *var;
-    t_list *elem;
+	t_var	*var;
+	t_list	*elem;
 
-    var = ft_calloc(1, sizeof(t_var));
-    if (!var)
-    {
-        load_var_error(ENV_VAR_ERROR, NULL, NULL);
-        return (0);
-    }
-    var->key = ft_strdup(key);
-    if (var->key && value)
-        var->data = ft_strdup(value);
-    if (!var->key || (value && !var->data))
-    {
-        load_var_error(ENV_VAR_ERROR, var, NULL);
-        return (0);
-    }
-    elem = ft_newlist(var);
-    if (!elem)
-    {
-        load_var_error(ENV_VAR_ERROR, var, NULL);
-        return (0);
-    }
-    ft_lstadd_back(var_list, elem);
-    return (1);
+	var = ft_calloc(1, sizeof(t_var));
+	if (!var)
+	{
+		load_var_error(ENV_VAR_ERROR, NULL, NULL);
+		return (0);
+	}
+	var->key = ft_strdup(key);
+	if (var->key && value)
+		var->data = ft_strdup(value);
+	if (!var->key || (value && !var->data))
+	{
+		load_var_error(ENV_VAR_ERROR, var, NULL);
+		return (0);
+	}
+	elem = ft_newlist(var);
+	if (!elem)
+	{
+		load_var_error(ENV_VAR_ERROR, var, NULL);
+		return (0);
+	}
+	ft_lstadd_back(var_list, elem);
+	return (1);
 }
 
-int edit_var(t_list **var_list, char *key, char *value)
+int	edit_var(t_list **var_list, char *key, char *value)
 {
-    t_var *var;
-    t_list *current;
+	t_var	*var;
+	t_list	*current;
 
-    current = *var_list;
-    while (current)
-    {
-        var = (t_var *)current->content;
-        if (!ft_strcmp(var->key, key))
-        {
-            if (value)
-            {
-                ft_safe_free(var->data);
-                var->data = ft_strdup(value);
-                if (!var->data)
-                {
-                    load_var_error(ENV_VAR_ERROR, NULL, NULL);
-                    return (0);
-                }
-            }
-            return (1);
-        }
-        current = current->next;
-    }
-    return (add_var(var_list, key, value));
+	current = *var_list;
+	while (current)
+	{
+		var = (t_var *)current->content;
+		if (!ft_strcmp(var->key, key))
+		{
+			if (value)
+			{
+				ft_safe_free(var->data);
+				var->data = ft_strdup(value);
+				if (!var->data)
+				{
+					load_var_error(ENV_VAR_ERROR, NULL, NULL);
+					return (0);
+				}
+			}
+			return (1);
+		}
+		current = current->next;
+	}
+	return (add_var(var_list, key, value));
 }
 
-t_var *create_var(char *str)
+t_var	*create_var(char *str)
 {
-    int i;
-    t_var *var;
+	int		i;
+	t_var	*var;
 
-    errno = 0;
-    var = ft_calloc(1, sizeof(t_var));
-    if (!var)
-        return (load_var_error(ENV_VAR_ERROR, NULL, NULL));
-    i = 0;
-    while (str[i] && str[i] != '=')
-        i++;
-    var->key = ft_substr(str, 0, i);
-    if (!var->key)
-        return (load_var_error(ENV_VAR_ERROR, var, NULL));
-    str += i + 1;
-    var->data = ft_strdup(str);
-    if (!var->data)
-        return (load_var_error(ENV_VAR_ERROR, var, NULL));
-    return (var);
+	errno = 0;
+	var = ft_calloc(1, sizeof(t_var));
+	if (!var)
+		return (load_var_error(ENV_VAR_ERROR, NULL, NULL));
+	i = 0;
+	while (str[i] && str[i] != '=')
+		i++;
+	var->key = ft_substr(str, 0, i);
+	if (!var->key)
+		return (load_var_error(ENV_VAR_ERROR, var, NULL));
+	str += i + 1;
+	var->data = ft_strdup(str);
+	if (!var->data)
+		return (load_var_error(ENV_VAR_ERROR, var, NULL));
+	return (var);
 }
 
-int build_var(char **envp, t_list **var_list)
+int	build_var(char **envp, t_list **var_list)
 {
-    t_var *var;
-    t_list *elem;
+	t_var	*var;
+	t_list	*elem;
 
-    *var_list = NULL;
-    while (*envp)
-    {
-        var = create_var(*envp);
-        if (var)
-            elem = ft_newlist(var);
-        if (!var || !elem)
-        {
-            ft_clearlst(var_list, free_var);
-            if (var)
-                (load_var_error(ENV_VAR_ERROR, var, NULL));
-            return (0);
-        }
-        ft_lstadd_back(var_list, elem);
-        envp++;
-    }
-    if (!edit_var(var_list, "?", "0"))
-    {
-        ft_clearlst(var_list, free_var);
-        return (0);
-    }
-    return (1);
+	*var_list = NULL;
+	while (*envp)
+	{
+		var = create_var(*envp);
+		if (var)
+			elem = ft_newlist(var);
+		if (!var || !elem)
+		{
+			ft_clearlst(var_list, free_var);
+			if (var)
+				(load_var_error(ENV_VAR_ERROR, var, NULL));
+			return (0);
+		}
+		ft_lstadd_back(var_list, elem);
+		envp++;
+	}
+	if (!edit_var(var_list, "?", "0"))
+	{
+		ft_clearlst(var_list, free_var);
+		return (0);
+	}
+	return (1);
 }
 
-char *search_var(t_list *var_list, char *key)
+char	*search_var(t_list *var_list, char *key)
 {
-    t_var *var;
+	t_var	*var;
 
-    while (var_list)
-    {
-        var = (t_var *)var_list->content;
-        if (!ft_strcmp(var->key, key))
-            return (var->data);
-        var_list = var_list->next;
-    }
-    return (NULL);
+	while (var_list)
+	{
+		var = (t_var *)var_list->content;
+		if (!ft_strcmp(var->key, key))
+			return (var->data);
+		var_list = var_list->next;
+	}
+	return (NULL);
 }
-
